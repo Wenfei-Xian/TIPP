2f8e59a4ffbb6b132ed8f8286663360194002b98
Wenfei Xian <2316469036@qq.com>
2023-11-16 11:04:31 +0100

    Add files via upload

--- TIPP_telomere.pl
+++ TIPP_telomere.pl
@@ -16,13 +16,15 @@ my$unit;
 my$fastq;
 my$contigs;
 my$bam;
-my$threads=40;
+my$threads=20;
 my$vcf_file_name;
 my$current_directory = cwd();
 my$contigs_name;
 my$fastq_name;
 my$extend=0;
-my$telomere_length=1000;
+my$minimum_uniq=2000;
+my$threads_spoa=2;
+my$two_telomere=1;
 
 GetOptions(
     'h'   => \$help,
@@ -33,7 +35,8 @@ GetOptions(
     'b=s' => \$bam,
     't=s' => \$threads,
     'e=s' => \$extend,
-    'l=s' => \$telomere_length,
+    'm=s' => \$minimum_uniq,
+    'p=s' => \$threads_spoa,
 );
 
 ###Help message
@@ -45,7 +48,7 @@ if ($help or not defined $vcf_file and not defined $unit and not defined $fastq 
     print "-e: extend the contigs with new assembled telomere sequences.(default=0, no extend)\n";
     print "-c: contigs. If the extension is not specified, it will be used as the output name.\n";
     print "-t: threads for minimap2.\n";
-    print "-l: telomere length used in seqtk -d.(default=1000)\n";
+    print "-m: minimum length of uniq sequence (without telomere)\n";
     exit;
 }
 
@@ -73,16 +76,77 @@ unless (-d "$contigs_name.telomere") {
 }
 
 ###Use seqtk to extract HiFi reads with telomeres unit.
-system("seqtk telo -m $unit $fastq -d $telomere_length > $contigs_name.telomere/$fastq_name.telo.fa");
+system("seqtk telo -m $unit $fastq  > $contigs_name.telomere/$fastq_name.telo.tmp.fa");
+
+if( $two_telomere == 1 ){
+	system("grep '>' $contigs_name.telomere/$fastq_name.telo.tmp.fa | sed 's/>//' | awk '{print \$1}'| sort | uniq -c |awk '{if(\$1 == 1)print \$2}' > $contigs_name.telomere/$fastq_name.telo.fa.single.ID");
+	system("seqtk subseq $contigs_name.telomere/$fastq_name.telo.tmp.fa $contigs_name.telomere/$fastq_name.telo.fa.single.ID > $contigs_name.telomere/$fastq_name.telo.fa");
+}
+else{
+	system("cp $contigs_name.telomere/$fastq_name.telo.tmp.fa $contigs_name.telomere/$fastq_name.telo.fa");
+}
 
 ###Process the seqtk output to extract and output sequences with telomeres removed.
 system("grep -A 1 'remove' --no-group-separator $contigs_name.telomere/$fastq_name.telo.fa > $contigs_name.telomere/$fastq_name.remove.telo.fa");
 
 ###Employ minimap2 to conduct an all-vs-all alignment of sequences with telomeres removed.
-system("minimap2 --eqx -c -x ava-pb -X -t $threads $contigs_name.telomere/$fastq_name.remove.telo.fa $contigs_name.telomere/$fastq_name.remove.telo.fa -o $contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf");
+system("minimap2 --eqx -c -x asm20 -X -t $threads $contigs_name.telomere/$fastq_name.remove.telo.fa $contigs_name.telomere/$fastq_name.remove.telo.fa -o $contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf");
 
 ###Extract potential read pairs stemming from the same telomere based on alignment results, treating each read as a node and each pair as an edge, for use as input to the MCL algorithm.
-system("awk '{if( ((\$10/\$11)>0.98) && (( (\$4-\$3)>\$2*0.9 && \$2>=1000 ) || ((\$9-\$8)>\$7*0.9 && \$7>=1000)) && (\$1 != \$6) ) print \$1\"\\t\"\$6}' $contigs_name.telomere/${fastq_name}.remove.telo.fa.self.paf | sort | uniq > $contigs_name.telomere/${fastq_name}.remove.telo.fa.self.paf.abc");
+open my$paf,"$contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf" or die "Can't open $contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf\n";
+open my$abc,">$contigs_name.telomere/${fastq_name}.remove.telo.fa.self.paf.abc" or die "Can't open $contigs_name.telomere/${fastq_name}.remove.telo.fa.self.paf.abc\n";
+my%paf_unit;
+my%paf_pair;
+while(<$paf>){
+	chomp;
+	my@array=split /\t/,$_;
+	if( !exists $paf_pair{"$array[0]\t$array[5]"} ){
+		$paf_pair{"$array[0]\t$array[5]"}=0;
+		if( $array[9]/$array[10] > 0.98  && $array[1] >= $minimum_uniq && $array[6] >= $minimum_uniq && $array[0] ne $array[5] ){
+			#MMMMMMMXMMMMMMMMMMMMMMMMM Q20
+			#MMMMMMMMMMMMXMMMMMMMMMMMM Q20
+			#0.99 * 0.99 = 0.98
+			if( $array[1] > $array[6] ){
+				#if( $array[2] <= 500 || $array[3] >= $array[1]-500 ){
+					if( $array[8]-$array[7] >= $array[6]*0.9 ){
+						if( ($array[2] == 0 && $array[7] == 0 && $array[8]+200 < $array[6]) || ( $array[1] == $array[3] && $array[6] == $array[8] && $array[7] > 200 ) || ( ($array[7] > 200) && ($array[8]+200 > $array[6] )) ){
+							#if the length of X, unmap sequence longer than the cutoff, two sequences will be considered to be origined from two telomeres
+							#
+							# MMMMMMMMMMMMMMMMMMMMMXXXXXX
+							# MMMMMMMMMMMMMMMMMMMMMXXXXXXX
+							#
+							# XXXXMMMMMMMMMMMMMMMMMMM
+							# XXXXMMMMMMMMMMMMMMMMMMM
+							#
+							# XXXXXMMMMMMMMMMMMXXXXXXXXXX
+							# XXXXXMMMMMMMMMMMXXXXXXX
+						}
+						else{
+							if( !exists $paf_unit{"$array[0]\t$array[5]"} ){
+								print $abc "$array[0]\t$array[5]\n";
+								$paf_unit{"$array[0]\t$array[5]"}=0;
+							}
+						}
+					}
+				#}
+			}
+			else{
+				#if( $array[7] <=500 || $array[8] >= $array[6]-500 ){
+					if( $array[3]-$array[2] >= $array[1]*0.9 ){
+						if( ($array[2] == 0 && $array[7] == 0 && $array[3]+200 < $array[1]) || ( $array[1] == $array[3] && $array[6] == $array[8] && $array[2] > 200 ) || ( ($array[2]>200) && ($array[3]+200 > $array[1] )) ){
+						}
+						else{
+							if( !exists $paf_unit{"$array[0]\t$array[5]"} ){
+								print $abc "$array[0]\t$array[5]\n";
+								$paf_unit{"$array[0]\t$array[5]"}=0;
+							}
+						}
+					}
+				#}
+			}
+		}
+	}
+}
 
 ###Perform clustering analysis using the MCL algorithm.
 system("mcl $contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf.abc --abc -o $contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf.abc.mcl");
@@ -97,11 +161,26 @@ my%hash_seq;
 while(<$telo>){
         chomp;
         my($ID,$seq)=(split /\n/,$_,2)[0,1];
-        $ID=(split / /,$ID)[0];
-        $ID=(split /\t/,$ID)[0];
-        $seq=~s/\n//g;
-        $seq=~s/>//;
-        $hash_seq{$ID}=$seq;
+	if( $ID=~m/_remove/){
+		next;
+	}
+	else{
+		my($id,$start_telo,$end_telo,$length_telo)=($1,$2,$3,$4) if($ID=~m/(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/);
+		$ID=(split / /,$ID)[0];
+		$ID=(split /\t/,$ID)[0];
+		$seq=~s/\n//g;
+		$seq=~s/>//;
+		#print "$id\n";
+		#print "$start_telo\n";
+		#print "$end_telo\n";
+		#print "$length_telo\n";
+		if( $length_telo-$end_telo < $start_telo ){
+			my$revcomp = reverse $seq;
+			$revcomp =~ tr/ACGTacgt/TGCAtgca/;
+			$seq=$revcomp;
+		}
+        	$hash_seq{$ID}=$seq;
+	}
 }
 $/="\n";
 close $telo;
@@ -112,19 +191,37 @@ open my$mcl,"$contigs_name.telomere/$fastq_name.remove.telo.fa.self.paf.abc.mcl"
 ###Create spoa command lines
 open my$spoa,">$contigs_name.telomere/$fastq_name.remove.telo.fa.spoa.sh";
 while(<$mcl>){
-        chomp;
-        open my$cluster,">$contigs_name.telomere/$fastq_name.telomere$telomere.fa";
-        my@reads=split /\t/,$_;
-        foreach my$read (@reads){
-                $read=~s/_remove//;###obtain the sequences with telomere
-                print $cluster ">$read\n$hash_seq{$read}\n";
-        }
-        print $spoa "spoa -r 2 -s $contigs_name.telomere/$fastq_name.telomere$telomere.fa > $contigs_name.telomere/$fastq_name.telomere$telomere.cons.MSA.fa\n";
+	chomp;
+	open my$cluster,">$contigs_name.telomere/$fastq_name.telomere$telomere.fa";
+	my@reads=split /\t/,$_;
+	foreach my$read (@reads){
+		$read=~s/_remove//;###obtain the sequences with telomere
+		print $cluster ">$read\n$hash_seq{$read}\n";
+	}
+	print $spoa "spoa -r 2 $contigs_name.telomere/$fastq_name.telomere$telomere.fa > $contigs_name.telomere/$fastq_name.telomere$telomere.cons.MSA.fa\n";
 	$telomere++;
 }
 
 ###Because SPOA can consume a high amount of memory, therefore only run two instances concurrently:
-system("cat $contigs_name.telomere/$fastq_name.remove.telo.fa.spoa.sh | xargs -I {} -P 2 bash -c {}");
+system("cat $contigs_name.telomere/$fastq_name.remove.telo.fa.spoa.sh | xargs -I {} -P $threads_spoa bash -c {}");
+
+sub calculate_overlap {
+    my ($start1, $end1, $start2, $end2) = @_;
+
+    # Sort the coordinates to make sure start is always less than end
+    ($start1, $end1) = ($start1 < $end1) ? ($start1, $end1) : ($end1, $start1);
+    ($start2, $end2) = ($start2 < $end2) ? ($start2, $end2) : ($end2, $start2);
+
+    # Find the maximum of the two starts and the minimum of the two ends
+    my $overlap_start = ($start1 > $start2) ? $start1 : $start2;
+    my $overlap_end = ($end1 < $end2) ? $end1 : $end2;
+
+    # Calculate overlap
+    my $overlap = $overlap_end - $overlap_start;
+
+    # If the overlap is negative, there is no overlap
+    return $overlap > 0 ? $overlap : 0;
+}
 
 ###Using a loop to process each telomere sequences.
 for (my $j=0; $j<$telomere; $j++) {
@@ -133,40 +230,50 @@ for (my $j=0; $j<$telomere; $j++) {
 	system("grep -A 1 'Consensus' $contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa | sed 's/>Consensus/>telomere$j/' | sed 's/-//g' > $contigs_name.telomere/$fastq_name.telomere$j.cons.fa");
 	
 	###For the convenience of later visualization, adjust the telomere sequences to the start of the sequence.
-	open(my$cons_seqtk, "-|", "seqtk telo -m $unit $fastq -d $telomere_length $contigs_name.telomere/$fastq_name.telomere$j.cons.fa | head -n 1") or die $!;
-	while (<$cons_seqtk>) {
-		chomp;
-		my@fields = split;
-
-		if ($fields[1] < $fields[3] / 2) { ###No change needed since the telomere is already at the beginning of the sequence.
+	system("seqtk telo -m $unit $contigs_name.telomere/$fastq_name.telomere$j.cons.fa 1> $contigs_name.telomere/$fastq_name.telomere$j.cons.fa.stat 2>$contigs_name.telomere/$fastq_name.telomere$j.cons.fa.stat2");
+	system("sed -i '1!d' \"$contigs_name.telomere/$fastq_name.telomere$j.cons.fa.stat\" ");
+	
+	open my$cons_seq, "$contigs_name.telomere/$fastq_name.telomere$j.cons.fa.stat" or die "Can't open $contigs_name.telomere/$fastq_name.telomere$j.cons.fa.stat";
+	$/ = "\n";
+	while (my$line=<$cons_seq>) {
+		chomp$line;
+		my@fields = split /\t/,$line;
+		my$half=$fields[3]/2;
+		my$overlap_start = calculate_overlap($fields[1], $fields[2], 0, $half);
+		my$overlap_end = calculate_overlap($fields[1], $fields[2], $half, $fields[3]);		
+		if ( $overlap_start >= $overlap_end ) { ###No change needed since the telomere is already at the beginning of the sequence.
 			system("cp $contigs_name.telomere/$fastq_name.telomere$j.cons.fa $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa");
 			system("trf $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa 2 5 7 80 10 50 2000 -l 10");
-			system("perl ${script_dir}html2repeatbed.pl $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.1.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.1.txt.html.bed");
+			system("mv $fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.txt.html $contigs_name.telomere");
+			system("mv $fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.html $contigs_name.telomere");
+			system("cat $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html");
+			system("perl ${script_dir}html2repeatbed.pl $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html.bed");
 		}
 		else{ ###Convert the sequence to its reverse complement.
 			my$filename_cons = "$contigs_name.telomere/$fastq_name.telomere$j.cons.fa";
 			my$out_filename_telomere_start = "$contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa";
-			open(my$in_cons, '<', $filename_cons) or die "Cannot open $filename_cons: $!";
-			open(my$out_cons_telomere_start, '>', $out_filename_telomere_start) or die "Cannot open $out_filename_telomere_start: $!";
+			open my$in_cons, $filename_cons or die "Cannot open $filename_cons\n";
+			open my$out_cons_telomere_start, ">$out_filename_telomere_start" or die "Cannot open $out_filename_telomere_start";
+			$/=">";<$in_cons>;
 			while (my$cons_line = <$in_cons>) {
-				chomp($cons_line);
-				if ($cons_line =~ /^>/) {
-					print $out_cons_telomere_start "$cons_line\n";
-				}
-				else {
-					my$revcomp = reverse $cons_line;
-					$revcomp =~ tr/ACGTacgt/TGCAtgca/;
-					print $out_cons_telomere_start "$revcomp\n";
-				}
+				my($ID_cons,$seq_cons)=(split /\n/,$cons_line,2)[0,1];
+				$seq_cons=~s/\n//g;
+				$seq_cons=~s/>//g;
+				my$revcomp = reverse $seq_cons;
+				$revcomp =~ tr/ACGTacgt/TGCAtgca/;
+				print $out_cons_telomere_start ">$ID_cons\n$revcomp\n";
 			}
+			$/="\n";
 			close($in_cons);
 			close($out_cons_telomere_start);
-			system("$contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa");
 			system("trf $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa 2 5 7 80 10 50 2000 -l 10");
-			system("perl ${script_dir}html2repeatbed.pl $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.1.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.1.txt.html.bed");
+			system("mv $fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.txt.html $contigs_name.telomere");
+			system("mv $fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.html $contigs_name.telomere");
+			system("cat $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.*.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html");
+			system("perl ${script_dir}html2repeatbed.pl $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html > $contigs_name.telomere/$fastq_name.telomere$j.cons.telomere_start.fa.2.5.7.80.10.50.2000.merged.txt.html.bed");
 		}
 	}
-	close($cons_seqtk);
+	close $cons_seq;
 
 	open my$MSA,"$contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa" or die "Can't open $contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa";
 	open my$MSA01,">$contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa.matrix" or die "Can't open $contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa.matrix";
@@ -193,9 +300,9 @@ for (my $j=0; $j<$telomere; $j++) {
 	system("Rscript ${script_dir}MSA.plot.r $contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa.matrix $contigs_name.telomere/$fastq_name.telomere$j.cons.MSA.fa.matrix.pdf");
 }
 
-system("cat $contigs_name.telomere/*bed > $contigs_name.telomere/$fastq_name.telomere.bed");
-system("cat $contigs_name.telomere/*cons.fa > $contigs_name.telomere/$fastq_name.telomere.cons.fa");
-system("Rscript ${script_dir}telomeres.visulization.r $contigs_name.telomere/$fastq_name.telomere.cons.fa $contigs_name.telomere/$fastq_name.telomere.bed $contigs_name.telomere/$fastq_name.telomere.pdf");
+system("cat $contigs_name.telomere/*bed > $contigs_name.telomere/$fastq_name.telomere.cons.telomere_start.bed");
+system("cat $contigs_name.telomere/*cons.telomere_start.fa > $contigs_name.telomere/$fastq_name.telomere.cons.telomere_start.fa");
+system("Rscript ${script_dir}telomeres.visulization.r $contigs_name.telomere/$fastq_name.telomere.cons.telomere_start.fa $contigs_name.telomere/$fastq_name.telomere.cons.telomere_start.bed $contigs_name.telomere/$fastq_name.telomere.cons.telomere_start.pdf");
 
 if( $telomere == 0 ){
 	print "   Sorry, no telomeres have been assembled. :(\n";
