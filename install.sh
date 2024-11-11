97c468b843a56fb913de5622d7d0203dd03a1dde
Wenfei Xian <2316469036@qq.com>
2024-11-11 11:43:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ cd ..
 # Compile the readskmercount program
 g++ -o readskmercount -I./kmc3 readskmercount.opt.cpp -L./kmc3/bin -lkmc_core -pthread
 
-CURRENT_PATH=$(pwd)/src
+CURRENT_PATH=$(pwd)
 echo "export PATH=$CURRENT_PATH:\$PATH" >> ~/.bashrc
 echo "export PATH=$CURRENT_PATH/seqtk:\$PATH" >> ~/.bashrc
 echo "export PATH=$CURRENT_PATH/kmc3/bin:\$PATH" >> ~/.bashrc
